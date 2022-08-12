d66077c6c3fce93dd099fde212c63c64c87c5ed3
vitmord <vitmord@gmail.com>
2022-08-12 13:50:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let slash = '|';
+
+    let closedBrackets = [];
+    let openedBrackets = [];
+    let rowBrackets = [];
+    let stack = [];
+
+    if (str.length % 2 !== 0) {
+        return false;
+
+    } else {
+        for (let elem of bracketsConfig) {
+            if (elem[0] === elem[1]) {
+
+                rowBrackets.push(elem[0]);
+            } else {
+
+                closedBrackets.push(elem[1]);
+
+                openedBrackets.push(elem[0]);
+            }
+        }
+        console.log(closedBrackets);
+        console.log(openedBrackets)
+
+        for (let i = 0; i < str.length; i++) {
+
+            let currentSymbol = str[i];
+            let topElement = stack[stack.length - 1];
+
+            if (openedBrackets.includes(currentSymbol)) {
+                stack.push(currentSymbol);
+
+
+
+            } else if (rowBrackets.includes(currentSymbol)) {
+
+                if (currentSymbol === topElement) {
+                    stack.pop();
+                } else {
+                    stack.push(currentSymbol);
+                }
+
+            } else {
+                if (openedBrackets[closedBrackets.indexOf(currentSymbol)] === topElement) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+
 }
